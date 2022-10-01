fe2e720ebeec78bbacd295ed2da2518853ba47ab
Adamizgoi <adamizgoi97@gmail.com>
2022-10-02 01:46:14 +0300

    BugFix 1 improv version restassured json-path

--- src/test/java/ru/netology/postman/PostmanEchoTest.java
+++ src/test/java/ru/netology/postman/PostmanEchoTest.java
@@ -9,18 +9,13 @@ public class PostmanEchoTest {
 
     @Test
     void shouldPostDemoAccount() {
-        // Given - When - Then
-// Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
-// Выполняемые действия
+                .body("This is expected to be sent back as part of response body.")
                 .when()
                 .post("/post")
-// Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
-        ;
+                .body("data", equalTo("This is expected to be sent back as part of response body."));
     }
 }
